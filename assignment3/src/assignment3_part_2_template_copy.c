2d60400dcc9a5a4c63efabf684739c86bdbe7b67
Vania Duarte Segura <vania.duarte@tum.de>
2022-05-17 11:17:14 +0200

    Changes in caprice routine and datatypes for k

--- assignment3/src/assignment3_part_2_template_copy.c
+++ assignment3/src/assignment3_part_2_template_copy.c
@@ -16,29 +16,13 @@ inline double get_duration(TimePoint t0, TimePoint t1) {
 struct entry {
     double v;
     int64_t next;
-    int u;
+    int u; //auxiliar variable for veryfing access
 };
 
-//Test if two numbers are coprime
-int coprime(int64_t num1, int num2)
-{
-    int min, count, flag = 1;
+/*
+ * TODO@Students: Q2a) Verify that all elements are accessed
+ */
 
-    min = num1 < num2 ? num1 : num2;
-
-    for(count = 2; count <= min; count++)
-    {
-        if( num1 % count == 0 && num2 % count == 0 )
-        {
-            flag = 0;
-            break;
-        }
-    }
-
-    return(flag);
-}
-
-//Check if all entries were accessed
 void verify_access(int64_t N, struct entry* A){
     for(int64_t i = 0; i<N; i++){
         if (A[i].u <1)
@@ -53,17 +37,17 @@ void verify_access(int64_t N, struct entry* A){
  * TODO@Students: Q2a) Implement the list initialization
  */
 
-void init(int64_t N, int k, struct entry* A) {
+void init(int64_t N, int64_t k, struct entry* A) {
     int64_t mask = N - 1; // N is power-of-2
     for (int64_t i = 0; i < N; ++i) {
         A[i].v = (double)i;
         A[i].next = (k * (i + 1)) & mask;
-        A[i].u = 0;
+        A[i].u = 0; 
     }
 }
 
 // sum_indexcalc_time = sum_indexcalc(N, 1, cycles, &sum_calc, &dummy, )
-double sum_indexcalc(int64_t N, int k, int REP, double *psum, int64_t* pdummy) {
+double sum_indexcalc(int64_t N, int64_t k, int REP, double *psum, int64_t* pdummy) {
     double sum;
     int64_t mask = (N - 1); // N is power - of -2
     int64_t dummy = 0;
@@ -71,7 +55,8 @@ double sum_indexcalc(int64_t N, int k, int REP, double *psum, int64_t* pdummy) {
     /*
      * TODO@Students: Q2a) Allocate and initialize data structures
      */
-
+    
+    //allocating memory for an object of size N*entry
     struct entry* A = (struct entry*) malloc(N*sizeof(struct entry));
     init(N, k, A);
     
@@ -97,13 +82,18 @@ double sum_indexcalc(int64_t N, int k, int REP, double *psum, int64_t* pdummy) {
      *   return average time per element access in nanoseconds
      */
      
+    //get duration from timers, and divide it by the product of REPS and N
+    //then multiply by 1.0e9 to get nanoseconds. 
     double average_time = (get_duration(t0, t1)/(REP*N)) * 1.0e+9;
+
+    //clean up data structures
     free(A);
+
     return average_time;
 }
 
 
-double sum_indexload(int64_t N, int k, int REP, double *psum, int64_t* pdummy) {
+double sum_indexload(int64_t N, int64_t k, int REP, double *psum, int64_t* pdummy) {
     double sum;
     int64_t mask = (N - 1); // N is power - of -2
     int64_t dummy = 0;
@@ -168,7 +158,7 @@ int main(int argc, char **argv) {
             maximumDatasetSize, totalNumberProcessedPoints
     );
 
-    printf("| %12s | %12s | %12s | %12s | %12s |\n", "Sum Type", "Data size", "Latency [ns]", "Cycles", "Checksum");
+    printf("| %12s | %12s |%12s | %12s | %12s | %12s |\n", "Sum Type", "K", "Data size", "Latency [ns]", "Cycles", "Checksum");
     
     /*                                                                                     /\
                                                                                           /  \
@@ -188,16 +178,15 @@ int main(int argc, char **argv) {
                                                                                        :__:-:__.;--'
                                                                                       '-'   '-'       */
     int64_t N = 1024; // =2^10 always a power of two
-    int k = 0;
+    int64_t k = 0;
     double Nn = 0.0;
-    double kk = 0.0;
     double sum_indexcalc_time=0, sum_indexload_time=0 ;
     double sum_calc=0, sum_load=0;
     int64_t cycles;
     int64_t dummy;
-    int gr = goldenRatio*10;
-    double goldenRatio_2 = gr / 10.0; 
-    printf("double goldenRatio_2 = %f\n", goldenRatio_2);
+    //int gr = goldenRatio*10;
+    //double goldenRatio_2 = gr / 10.0; 
+    //printf("double goldenRatio_2 = %f\n", goldenRatio_2);
     
 
     while (N <= maximumDatasetSize) 
@@ -205,73 +194,47 @@ int main(int argc, char **argv) {
         cycles = std::clamp(totalNumberProcessedPoints / N, (int64_t)1, (int64_t)1000);
 
         sum_indexcalc_time = sum_indexcalc(N, 1, cycles, &sum_calc, &dummy);
-        printf( "| %12s | %12ld | %12.2f | %12ld | %12.1f |  \n", "sum_indexcalc k=1",N, sum_indexcalc_time, cycles, sum_calc);
+        printf("| %12s |  1  | %12ld | %12.2f | %12ld | %12.1f |  \n", "sum_indexcalc k=1",N, sum_indexcalc_time, cycles, sum_calc);
         sum_indexload_time = sum_indexload(N, 1, cycles, &sum_load, &dummy);
-        printf("| %12s | %12ld | %12.2f | %12ld | %12.1f |  \n", "sum_indexload k=1",N, sum_indexload_time, cycles, sum_load );
+        printf("| %12s |  1  | %12ld | %12.2f | %12ld | %12.1f |  \n", "sum_indexload k=1",N, sum_indexload_time, cycles, sum_load );
 
         /*
          * TODO@Students: Q2a) make sure N and k are coprime and set the k so that N/k is close to golden ratio - for pseudo-randomness
          */
-
-        /*for (k = 3;;k++)
-        {   
-            Nn = (double)N;  
-            kk = (double)k;
-
-            double error = abs(goldenRatio - (Nn/kk)); //remove ceiling
-            
-            if ((k % 2 != 0) && error < 0.75)
-            {
-                printf("chosen k = %d with N/K =%f\n", k, Nn/kk);
-                
-                break;
-            }
-            if (k > 10000000)
-            {
-                printf("k was not found");
-                exit(1);
-            }
-
-        }*/
-
-        // with ceiling and using rounded version
+         
+    
+        /*
+        two integers a and b are coprime, 
+        relatively prime or mutually prime if the only positive integer 
+        that is a divisor of both of them is 1. Since N is a power of 2, we tested all numbers k
+        that aren't multiples of 2 and whose ratio with N is close to the golden radio within
+        a tolerance of 0.4. 
+        We take the ceiling of this ratio to avoid effects of integer division (i. e rounding to smallest integer) 
+        
+        */ 
         for (k = 3;;k++)
         {   Nn = (double)N;
-            double error = abs(goldenRatio_2 - ceil(Nn/k));
+            double error = abs(goldenRatio - ceil(Nn/k));
             
-            if ((k % 2 != 0) && error < 1.0)
+            if ((k % 2 != 0) && error < 0.4)
             {
-                printf("chosen k = %d with N/K =%f\n", k, Nn/k);
+                //printf("chosen k = %ld with N/K =%f\n", k, Nn/k);
                 
                 break;
             }
-            if (k > 10000000)
+            if (k > 10000000000000)
             {
                 printf("k was not found");
                 exit(1);
             }
 
         }
-
-
-
-        
-
-        
         
          
         sum_indexcalc_time = sum_indexcalc(N, k, cycles, &sum_calc, &dummy);
-        printf("| %12s | %12ld | %12.2f | %12ld | %12.1f | \n", "sum_indexcalc k=gold", N, sum_indexcalc_time, cycles, sum_calc);
+        printf("| %12s | %12ld |%12ld | %12.2f | %12ld | %12.1f | \n", "sum_indexcalc k=gold", k, N, sum_indexcalc_time, cycles, sum_calc);
         sum_indexload_time = sum_indexload(N, k, cycles, &sum_load, &dummy);
-        printf("| %12s | %12ld | %12.2f | %12ld | %12.1f | \n", "sum_indexload k=gold", N, sum_indexload_time, cycles, sum_load );
-
-        
-
-            
-
-        /*
-         * TODO@Students: Q2a) Verify that all elements are accessed
-         */
+        printf("| %12s | %12ld | %12ld | %12.2f | %12ld | %12.1f | \n", "sum_indexload k=gold", k, N, sum_indexload_time, cycles, sum_load );
 
         N *= 2;
     }
